337eebd45835e6645921cd004e0e99665c78e1c9
Aidan Middleton <aidanmiddleton@Aidans-MacBook-Pro.local>
2020-10-05 10:29:19 -0700

    first commit

---
+++ VariableTypes.java
@@ -0,0 +1,26 @@
+public class VariableTypes {
+
+    public static void main(String[] args) {
+
+        byte verySmallNum = 120;
+
+        final short slightlyBiggerNum = 300;
+
+        int evenBiggerNum = 200000;
+
+        long biggestInt = 200000000;
+
+        float decimalNum = 9.15f;
+
+        double bigDecimalNum = 8.9999999;
+
+        boolean isTrue = true;
+
+        char letter = 'a';
+
+        System.out.println(slightlyBiggerNum);
+
+
+    }
+
+}
